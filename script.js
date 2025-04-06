b154474464b2fc59f2ffbcfda135a2b814a0b49d
Chayanit Chansuttikanok <baitoey8344@gmail.com>
2025-04-06 17:01:30 +0700

    uppdate

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ cat.addEventListener('mousedown', () => {
 });
 
 cat.addEventListener('mouseup', () => {
-  cat.src = 'https://pngmax.com/_next/image?url=https%3A%2F%2Fpng-max.s3.ap-south-1.amazonaws.com%2Flow%2F936505e8-677a-4a6a-af4e-e4a8c0f31dc9.png&w=1200&q=75'; // ภาพแมวปากปิด
+  cat.src = 'https://pngmax.com/_next/image?url=https%3A%2F%2Fpng-max.s3.ap-south-1.amazonaws.com%2Flow%2F936505e8-677a-4a6a-af4e-e4a8c0f31dc9.png&w=1200&q=75';
 });
 
 function resetCount(){
